a8489d1696d3ab18b1bb60a115b9c6d32c922967
root <root@localhost>
2016-08-18 22:13:40 +0300

    add 1

--- TheatreSquare/main.cpp
+++ TheatreSquare/main.cpp
@@ -5,13 +5,14 @@
 using std::string;
 
 string multy(string a, string b);
-//string sum(string a, string b);
+//string sum(string a, string b, int i);
 
 
 int main(unsigned  int argc, char* argv[])
 {
-	char a[100] = "257";
-	char b[100] = "25";
+	char a[100] = "257";//175 + 125 + 50
+	char b[100] = "25"; 
+	//175 + 1250 + 5000 = 6425
 	string t = multy(a,b);
 
 	std::cout << t  << std::endl;
@@ -26,21 +27,26 @@ string multy(string a, string b) {
 	int t = 0;
 
 	
-	for (int i = a.length(); i; i--)
+	for (int i = a.length(); i ; i--)
 	{
 		for (int j = b.length(); j; j--, t++)
 		{
-			
-			mem = ((a[i - 1] - '0') * (b[j - 1] - '0')) + mem / 10;
+			if(a.length()){
+				for(int n = 0; n < i;n++)
+					buffer += std::to_string(0);
+			}
+mem =  ((a[i - 1] - '0') * (b[j - 1] - '0')) + mem / 10;
 			if ((j - 1) == 0) {
 				
 				buffer += std::to_string(mem % 10);
 			if(mem > 10)
 				buffer += std::to_string(mem / 10);
+				buffer += '+';
 			}
 			else {		
 				buffer += std::to_string(mem % 10);			
 			}
+			
 		}
 
 		mem = 0;
@@ -50,7 +56,9 @@ string multy(string a, string b) {
 	return buffer;
 }
 /*
-string sum(string a, string b) {
-
+string sum(string a, string b, int i) {
+	string equal;
+	equal +
+	
 }
 */
