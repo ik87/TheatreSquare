c93b8de9ae45c5633cf3070f37b113393be59a08
worldown <d_dexter@mail.ru>
2016-08-29 17:45:27 +0300

    E. Generate a String, v1.1

--- TheatreSquare/main5.cpp
+++ TheatreSquare/main5.cpp
@@ -1,14 +1,20 @@
-//E. Generate a String, v1.0
+//E. Generate a String, v1.1
 #include <iostream>
 using namespace std;
 
-int main(){
-	unsigned long n,x,y;
-	n = 7; x = 99; y = 62;
-//n = 6; x = 5 ; y = 16;
+int testBit(unsigned long n);
+int countN(unsigned long n);
 
-	bool xBig = x > y;
-	int i = 0, j = 0;
+int main(){
+	unsigned long n,x,y, size;
+	//n = 10; x = 99; y = 62;
+	//n = 6; x = 5 ; y = 16;
+	//n = 1; x = 10; y = 1;
+	//n = 8, x = 4, y = 8;
+	n = 57, x = 5289, y = 8444;
+
+	//size = 
+	int i = 0, j = 0, pX = 0, pY = 0;
 	// 99+62+62+99+62 = 384
 	//1010	101	10	1
 	//10		 5		 2	 1
@@ -18,15 +24,32 @@ int main(){
 	
 	
 	while(n){
-	
+
+
+		switch (testBit(n))
+		{
+			case 1: i++; n++; break;
+			case -1: i++; n--; break;
+		}
+
+
 		if(n & 1 ){ // if 1
 			i++; // 1
 			n = n >> 1;
 			n = n << 1;
 		}
-	else{
-		j++;
-		n = n >> 1;
+		else{
+			//if (n % 2 == 0 && n != 1) {
+				if ((pX = (n * x) ) >= (pY = (y * countN(n)))) { //4, 8
+					j++;
+					n = n >> 1;
+				}
+				else {
+					i += n - (n >> 1);
+					n = n >> 1;
+				}
+			//}
+
 	}
 
 	
@@ -35,5 +58,29 @@ int main(){
 	}
 	
 	cout << "i " << i << "; j " << j << endl;
+	cout << (i * x) + (j * y) << endl;
 	return 0;
+}
+
+int testBit(unsigned long n) {
+	int bit = sizeof(n) * 8;
+	
+	for (size_t i = 2; i < bit; i++)
+	{
+		if ((n + 1) == 1 << i)
+			return 1;
+		if ((n - 1) == 1 << i)
+			return -1;
+	}
+	return 0;
+}
+int countN(unsigned long n) {
+	if (n == 4)
+		return 2;
+	int bit = (sizeof(n) * 8);
+	for (size_t i = bit; i; i--)
+	{
+		if ( n & 1 << i)
+			return i;
+	}
 }
