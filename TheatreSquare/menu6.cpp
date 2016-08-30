7dbb2bb01941a582f1d8b518e53464611e5a79e4
worldown <d_dexter@mail.ru>
2016-08-30 18:07:58 +0300

    E. Generate a String, v1.4

--- TheatreSquare/menu6.cpp
+++ TheatreSquare/menu6.cpp
@@ -1,24 +1,21 @@
-//E. Generate a String, v1.3
+//E. Generate a String, v1.4
 #include <iostream>
-//#include <string>
+#include <string>
 using namespace std;
 
 class Branch {
-private:
+public:
+	unsigned long long n, x, y;
 	int i;
 	int j;
-	unsigned long n, x, y;
-
-
-public:
-//	string patch;
-	Branch(unsigned long n, unsigned long x, unsigned long y) {
+	string patch;
+	Branch(unsigned long long n, unsigned long long x, unsigned long long y) {
 		this->n = n;
 		this->i = 0;
 		this->j = 0;
 		this->x = x;
 		this->y = y;
-	//	this->patch = std::to_string(n);
+		this->patch.append(to_string(n));
 	}
 	Branch(Branch *br)
 	{
@@ -27,64 +24,78 @@ public:
 		this->j = br->j;
 		this->x = br->x;
 		this->y = br->y;
+		this->patch = br->patch;
 
 
 	}
 	~Branch() {}
 
-	unsigned long getN() { return n; }
+	unsigned long long getN() { return n; }
 
 	Branch plusN() { n++; i++;  return this; }
 	Branch minusN() { n--; i++; return this; }
+	Branch minusEND() { i += (int)n - 1; n = 1; return this; }
 	Branch halfN() { n = n >> 1; j++;   return this; }
-//	void addPatch() { patch += to_string(int(n)); }
-	unsigned long getTotalSec() { return ((i*x) + (j*y)); }
-	bool yLanger() {return (y > x);}
+	unsigned long long getTotalSec() { return ((i*x) + (j*y)); }
+	bool yLanger() { return y > x;  }
+	void upPatch() { patch.append(" ");  patch.append(to_string(n)); }
 
 };
 
 
 
-void factory(Branch br, unsigned long &min) {
-	
+void factory(Branch br, unsigned long long &min, unsigned long long &sum) {
+	br.upPatch();
 
 	if (br.getN() == 1) {
 		br.minusN();
 		//br.minusN();
 		if (min > br.getTotalSec())
 			min = br.getTotalSec();
-		
+		sum++;
+		cout << br.patch << "\t" <<br.getTotalSec() << endl;
+		cout << " i "<< br.i << " j: " << br.j << endl;
+
 		return;
 	}
+
 	if (br.getN() & 1) {
 		Branch brL(br);
 		Branch brR(br);
-		factory(brR.plusN(), min);
-		factory(brL.minusN(), min);
+		factory(brR.plusN(), min, sum);
+		factory(brL.minusN(), min, sum);
 
 
 	}
 	else {
-		if(br.getN() == 2 && br.yLanger())
-			factory(br.minusN(), min);
+		if (br.n > (br.y/br.x)*2 )
+			factory(br.halfN(), min, sum);
 		else
-			factory(br.halfN(), min);
+			factory(br.minusEND(), min, sum);
+		
 	}
+
 }
 
 int main() {
-	unsigned long n, x, y;
+	unsigned long long n, x, y, sum;
 	//n = 10; x = 99; y = 62;
+	sum = 0;
 	//n = 6; x = 5 ; y = 16;
 	//n = 1; x = 10; y = 1;
 	//n = 8, x = 4, y = 8;
-	n = 57, x = 5289, y = 8444;
-	//n = 8; x = 1; y = 1;
-	unsigned long min = 0;
+	//n = 57, x = 5289, y = 8444; //excpected 2519291691
+	//cin >> n >> x >> y;
+	//n = 9; x = 1; y = 10;
+	n = 382; x = 81437847; y = 324871127;
+	//n = 244; x = 575154303; y = 436759189;
+
+	unsigned long long min = 0;
 	min--;
 	Branch br(n, x, y);
-	factory(br, min);
+	factory(br, min, sum);
 	cout << min << endl;
+	cout << sum << endl;
 	return 0;
 }
 /*9 = 8+1 4 2 1
